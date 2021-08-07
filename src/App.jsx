4891c63ae90ec4691a30180ff77e501d09a4b7f1
coffmanjrp <coffmanjrp@gmail.com>
2021-08-07 15:50:08 -0700

    Add on click functionon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useRef, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import './App.scss';
 
 function App() {
+  const [active, setActive] = useState(false);
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    setActive((prev) => !prev);
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="search active">
-      <input type="text" className="input" placeholder="Search..." />
-      <button className="btn">
+    <div className={`search${active ? ' active' : ''}`}>
+      <input
+        type="text"
+        ref={inputRef}
+        className="input"
+        placeholder="Search..."
+      />
+      <button className="btn" onClick={handleClick}>
         <FaSearch />
       </button>
     </div>
